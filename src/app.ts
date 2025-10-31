8a0566510f6ba25d8c9a9e89e8fa7beebcb19f76
maertin <mutuverria00@gmail.com>
2025-10-30 22:13:48 -0300

    actualiza package para deploy

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,17 @@
-import 'reflect-metadata';
-import { envs } from './config/envs';
-import { AppRoutes } from './routes/router';
-import { Server } from './server/server';
+import "reflect-metadata";
+import { envs } from "./config/envs";
+import { AppRoutes } from "./routes/router";
+import { Server } from "./server/server";
 
 (async () => {
-main();
+  main();
 })();
 
-
-function main(){
-    const server = new Server(
-        {
-            port: envs.PORT,
-            routes: AppRoutes.routes
-        }
-    );
-    server.start();
-    console.log('Servidor iniciado');
+function main() {
+  const server = new Server({
+    port: envs.PORT || 3000,
+    routes: AppRoutes.routes,
+  });
+  server.start();
+  console.log("Servidor iniciado");
 }
